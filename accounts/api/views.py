ba5cf8bfe13d7a704c120f55f392c6c71321d5b5
Sibing <1915563868@qq.com>
2021-08-03 21:12:34 +0800

    Add login, logout, signup api for user

--- accounts/api/views.py
+++ accounts/api/views.py
@@ -1,7 +1,15 @@
-from django.contrib.auth.models import User, Group
+from django.contrib.auth.models import User
+from rest_framework import status
 from rest_framework import viewsets
 from rest_framework import permissions
-from accounts.api.serializers import UserSerializer
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from accounts.api.serializers import UserSerializer, LoginSerializer, SignupSerializer
+from django.contrib.auth import (
+    authenticate as django_authenticate,
+    login as django_login,
+    logout as django_logout,
+)
 
 
 class UserViewSet(viewsets.ModelViewSet):
@@ -11,3 +19,88 @@ class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all().order_by('-date_joined')
     serializer_class = UserSerializer
     permission_classes = [permissions.IsAuthenticated]
+
+class AccountViewSet(viewsets.ViewSet):
+    serializer_class = SignupSerializer
+
+    @action(methods=['GET'], detail=False)
+    def login_status(self, request):
+        """
+        查看用户当前的登录状态和具体信息
+        """
+        data = {'has_logged_in': request.user.is_authenticated}
+        if request.user.is_authenticated:
+            data['user'] = UserSerializer(request.user).data
+        return Response(data)
+
+    @action(methods=['POST'], detail=False)
+    def login(self, request):
+        """
+        使用 username, password 进行注册
+        默认的 username 是 admin, password 也是 admin
+        """
+        # get username and password from request
+        serializer = LoginSerializer(data=request.data)
+
+        # validate input
+        if not serializer.is_valid():
+            return Response({
+                'success': False,
+                'message': 'Please check input.',
+                'errors': serializer.errors,
+            }, status=status.HTTP_400_BAD_REQUEST)
+
+        # validation ok, login
+        username = serializer.validated_data['username']
+        password = serializer.validated_data['password']
+
+        # check if user exists
+        if not User.objects.filter(username=username).exists():
+            return Response({
+                'success': False,
+                'message': 'User does not exists.',
+            }, status=status.HTTP_400_BAD_REQUEST)
+
+        # check if given credentials are valid
+        user = django_authenticate(username=username, password=password)
+        if not user or user.is_anonymous:
+            return Response({
+                'success': False,
+                'message': 'Username and password does not match.',
+            }, status=status.HTTP_400_BAD_REQUEST)
+
+        # log in
+        django_login(request, user)
+        return Response({
+            'success': True,
+            'user': UserSerializer(instance=user).data,
+        })
+
+
+    @action(methods=['POST'], detail=False)
+    def logout(self, request):
+        """
+        登出当前用户
+        """
+        django_logout(request)
+        return Response({'success': True})
+
+    @action(methods=['POST'], detail=False)
+    def signup(self, request):
+        """
+        使用 username, email, password 进行注册
+        """
+        serializer = SignupSerializer(data=request.data)
+        if not serializer.is_valid():
+            return Response({
+                'success': False,
+                'message': 'Please check input.',
+                'errors': serializer.errors,
+            }, status=status.HTTP_400_BAD_REQUEST)
+
+        user = serializer.save()
+        django_login(request, user)
+        return Response({
+            'success': True,
+            'user': UserSerializer(instance=user).data,
+        }, status=status.HTTP_201_CREATED)
