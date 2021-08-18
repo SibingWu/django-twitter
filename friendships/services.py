c6f8573cbe43190630a306f25726d8c6ef752274
Sibing <1915563868@qq.com>
2021-08-19 00:12:40 +0800

    Add newsfeeds fanout logic

---
+++ friendships/services.py
@@ -0,0 +1,14 @@
+from friendships.models import Friendship
+
+
+class FriendshipService:
+
+    @classmethod
+    def get_followers(cls, user):
+        # friendships = Friendship.objects.filter(to_user=user)
+        # follower_ids = [friendship.from_user_id for friendship in friendships]
+        # followers = User.objects.filter(id__in=follower_ids)
+
+        friendships = Friendship.objects.filter(to_user=user).prefetch_related('from_user')
+        followers = [friendship.from_user for friendship in friendships]
+        return followers
