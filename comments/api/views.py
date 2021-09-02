99f9d578ef25053094345869af5d6afddd7daefe
Sibing <1915563868@qq.com>
2021-09-02 22:41:30 +0800

    Support update and destroy comments

--- comments/api/views.py
+++ comments/api/views.py
@@ -3,8 +3,10 @@ from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.request import Request
 from rest_framework.response import Response
 
+from comments.api.permissions import IsObjectOwner
 from comments.api.serializers import (
     CommentSerializerForCreate,
+    CommentSerializerForUpdate,
     CommentSerializer,
 )
 from comments.models import Comment
@@ -23,9 +25,14 @@ class CommentViewSet(viewsets.GenericViewSet):
         # 而不是 AllowAny / IsAuthenticated 这样只是一个类名
         if self.action == 'create':
             return [IsAuthenticated()]
+        if self.action in ['destroy', 'update']:
+            return [IsAuthenticated(), IsObjectOwner()]
         return [AllowAny()]
 
     def create(self, request: Request):
+        """
+        POST /api/comments/
+        """
         data = {
             'user_id': request.user.id,
             'tweet_id': request.data.get('tweet_id'),
@@ -46,3 +53,47 @@ class CommentViewSet(viewsets.GenericViewSet):
             data=CommentSerializer(comment).data,
             status=status.HTTP_201_CREATED,
         )
+
+    def update(self, request: Request, *args, **kwargs):
+        """
+        PUT /api/comments/<pk>/
+        """
+        # 需有 **kwards 以接收调用 update 的 pk
+
+        # get_object 是 DRF 包装的一个函数，是基于queryset的设定
+        # 会在找不到的时候 raise 404 error
+        # 所以这里无需做额外判断
+        comment = self.get_object()
+        serializer = CommentSerializerForUpdate(
+            # 指定 instance 时，会调用 serializer 的 update 操作
+            # 当 instance 不存在时，会调用 serializer 的 create 操作
+            instance=comment,
+            data=request.data,
+        )
+
+        # validate input
+        if not serializer.is_valid():
+            return Response({
+                'message': 'Please check input',
+                'errors': serializer.errors,
+            }, status=status.HTTP_400_BAD_REQUEST)
+
+        # save 方法会触发 serializer 里的 update 方法，点进 save 的具体实现里可以看到
+        # save 是根据 instance 参数有没有传来决定是触发 create 还是 update
+        comment = serializer.save()
+        return Response(
+            data=CommentSerializer(comment).data,
+            status=status.HTTP_200_OK,
+        )
+
+    def destroy(self, request: Request, *args, **kwargs):
+        """
+        DELETE /api/comments/<pk>/
+        """
+        comment = self.get_object()
+        comment.delete()
+        # DRF 里默认 destroy 返回的是 status code = 204 no content
+        # 这里 return 了 success=True 更直观的让前端去做判断，所以 return 200 更合适
+        return Response({
+            'success': True,
+        }, status=status.HTTP_200_OK)
