73c159488a6a8e4460f7f1084fd80b18ef9d43e5
Sibing <1915563868@qq.com>
2021-11-03 23:38:54 +0800

    Change list api to go through cache and cache newsfeed list in redis

--- newsfeeds/api/views.py
+++ newsfeeds/api/views.py
@@ -2,7 +2,7 @@ from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.request import Request
 from newsfeeds.api.serializers import NewsFeedSerializer
-from newsfeeds.models import NewsFeed
+from newsfeeds.services import NewsFeedService
 from utils.paginations import EndlessPagination
 
 
@@ -10,16 +10,18 @@ class NewsFeedViewSet(viewsets.GenericViewSet):
     permission_classes = [IsAuthenticated]
     pagination_class = EndlessPagination
 
-    def get_queryset(self):
-        # 重载以自定义 queryset，因为 newsfeed 的查看是有权限的
-        # 只能看 user=当前登录用户的 newsfeed
-        return NewsFeed.objects.filter(user_id=self.request.user.id)
+    # def get_queryset(self):
+    #     # 重载以自定义 queryset，因为 newsfeed 的查看是有权限的
+    #     # 只能看 user=当前登录用户的 newsfeed
+    #     return NewsFeed.objects.filter(user_id=self.request.user.id)
 
     def list(self, request: Request):
         """
         GET /api/newsfeeds/
         """
-        page = self.paginate_queryset(self.get_queryset())
+        # page = self.paginate_queryset(self.get_queryset())
+        newsfeeds = NewsFeedService.get_cached_newsfeeds(request.user.id)
+        page = self.paginate_queryset(newsfeeds)
         serializer = NewsFeedSerializer(
             instance=page,
             context={'request': request},
--- newsfeeds/tests.py
+++ newsfeeds/tests.py
@@ -1,3 +1,51 @@
-from django.test import TestCase
+from newsfeeds.services import NewsFeedService
+from testing.testcases import TestCase
+
+from twitter.cache import USER_NEWSFEEDS_PATTERN
+from utils.redis_client import RedisClient
+
 
 # Create your tests here.
+class NewsFeedServiceTests(TestCase):
+
+    def setUp(self):
+        self.clear_cache()
+        self.lisa = self.create_user('lisa')
+        self.emma = self.create_user('emma')
+
+    def test_get_user_newsfeeds(self):
+        newsfeed_ids = []
+        for i in range(3):
+            tweet = self.create_tweet(self.emma)
+            newsfeed = self.create_newsfeed(self.lisa, tweet)
+            newsfeed_ids.append(newsfeed.id)
+        newsfeed_ids = newsfeed_ids[::-1]
+
+        # cache miss
+        newsfeeds = NewsFeedService.get_cached_newsfeeds(self.lisa.id)
+        self.assertEqual([f.id for f in newsfeeds], newsfeed_ids)
+
+        # cache hit
+        newsfeeds = NewsFeedService.get_cached_newsfeeds(self.lisa.id)
+        self.assertEqual([f.id for f in newsfeeds], newsfeed_ids)
+
+        # cache updated
+        tweet = self.create_tweet(self.lisa)
+        new_newsfeed = self.create_newsfeed(self.lisa, tweet)
+        newsfeeds = NewsFeedService.get_cached_newsfeeds(self.lisa.id)
+        newsfeed_ids.insert(0, new_newsfeed.id)
+        self.assertEqual([f.id for f in newsfeeds], newsfeed_ids)
+
+    def test_create_new_newsfeed_before_get_cached_newsfeeds(self):
+        feed1 = self.create_newsfeed(self.lisa, self.create_tweet(self.lisa))
+
+        RedisClient.clear()
+        conn = RedisClient.get_connection()
+
+        key = USER_NEWSFEEDS_PATTERN.format(user_id=self.lisa.id)
+        self.assertEqual(conn.exists(key), False)
+        feed2 = self.create_newsfeed(self.lisa, self.create_tweet(self.lisa))
+        self.assertEqual(conn.exists(key), True)
+
+        feeds = NewsFeedService.get_cached_newsfeeds(self.lisa.id)
+        self.assertEqual([f.id for f in feeds], [feed2.id, feed1.id])
