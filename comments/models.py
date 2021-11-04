8a5f138c214c1952463ab43e4045b990f351dcd6
Sibing <1915563868@qq.com>
2021-11-04 12:08:46 +0800

    Denormalize likes_count and comments_count

--- comments/models.py
+++ comments/models.py
@@ -1,7 +1,9 @@
 from django.contrib.auth.models import User
 from django.contrib.contenttypes.models import ContentType
 from django.db import models
+from django.db.models.signals import post_save, pre_delete
 
+from comments.listeners import incr_comments_count, decr_comments_count
 from likes.models import Like
 from tweets.models import Tweet
 from utils.memcached_helper import MemcachedHelper
@@ -40,3 +42,7 @@ class Comment(models.Model):
     @property
     def cached_user(self):
         return MemcachedHelper.get_object_through_cache(User, self.user_id)
+
+
+post_save.connect(incr_comments_count, sender=Comment)
+pre_delete.connect(decr_comments_count, sender=Comment)
